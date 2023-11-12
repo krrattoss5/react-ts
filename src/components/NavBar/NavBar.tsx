03db789e764a0c7690c0259b7f9a5623fa54987e
krrattoss5 <krrattoss5@gmail.com>
2023-11-12 02:32:18 -0500

    (crear tarea terminado, barra de navegacion funcional)

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,18 @@
 import {FC} from 'react'
 import s from './NavBar.module.css'
 
-const NavBar: FC= ()=>{
+interface NavBarProps {
+  pushFlag: (flag: string) => void;
+}
+
+const NavBar: FC<NavBarProps> = ({ pushFlag }) => {
   return(
     <div className={s.container}>
       <div className={s.nav}>
-        <p className={s.buttonNav}>Nueva Tarea</p>
-        <p className={s.buttonNav}>Finalizado</p>
-        <p className={s.buttonNav}>Pendientes</p>
+        <p onClick={()=>pushFlag('home')} className={s.buttonNav}>Inicio</p>
+        <p onClick={()=>pushFlag('form')} className={s.buttonNav}>Nueva Tarea</p>
+        <p onClick={()=>pushFlag('form')} className={s.buttonNav}>Finalizado</p>
+        <p onClick={()=>pushFlag('form')} className={s.buttonNav}>Pendientes</p>
       </div>
     </div>
   )
